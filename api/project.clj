07e921cbe6535b4fe93e29ddc0e9d1009ed321ff
Stuart Quin <stuart.quin@gmail.com>
2013-10-06 10:20:13 +0100

    Remove dependencies

--- api/project.clj
+++ api/project.clj
@@ -3,9 +3,6 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-json "0.1.2"]
-                 [c3p0/c3p0 "0.9.1.2"]
-                 [org.clojure/java.jdbc "0.2.3"]
-                 [com.h2database/h2 "1.3.168"]
                  [cheshire "5.1.1"]
                  [compojure "1.1.5"]
                  [clj-time "0.6.0"]
