244bb05f45979865bdcfe31ef2da2273c5a585f3
Stuart Quin <stuart.quin@gmail.com>
2013-10-06 12:13:58 +0100

    Clientside frameworks and tests

--- api/test/clojure_rest/test/handler.clj
+++ api/test/clojure_rest/test/handler.clj
@@ -1,9 +1,14 @@
 (ns clojure-rest.test.handler
   (:use clojure.test
         ring.mock.request  
-        clojure-rest.handler))
+        clojure-rest.handler
+        clojure.contrib.core
+        clojure.contrib.mock.test-adapter)
+  (:require [clojure.test :as test]))
+
 
 (deftest test-app
   (testing "Entries path"
-    (let [response (app (request :get "/entries"))]
-      (is (= (:status response) 200)))))
+    (expect [mc/find-maps (times once (has-args "entries"))]
+      (let [response (app (request :get "/entries"))]
+        (is (= (:status response) 200))))))
