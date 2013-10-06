91bda928466f7d9230e0ae7bc12a5b83e359b0f8
Stuart Quin <stuart.quin@gmail.com>
2013-10-06 18:39:15 +0100

    Add new entries

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -6,10 +6,13 @@ angular.module('WorkingOnApp')
       $scope.entries = data.results;
     };
 
-    var Entries = $resource("http://local.workingon.com/entries");
-    Entries.get({}, setEntriesScope);
+    var Entry = $resource("http://local.workingon.com/entries");
+    Entry.get({}, setEntriesScope);
 
     $scope.createEntry = function(){
+      var entry = new Entry();
+      entry.text = this.newEntry;
+      entry.$save();
       $scope.entries.unshift({created_at: (new Date()).getTime(), text: this.newEntry});
     };
   });
