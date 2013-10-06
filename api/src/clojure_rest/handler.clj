dac1a6343dcfb9333c2be61779a6a16ee57850df
Stuart Quin <stuart.quin@gmail.com>
2013-10-06 18:52:46 +0100

    Spacing change

--- api/src/clojure_rest/handler.clj
+++ api/src/clojure_rest/handler.clj
@@ -1,44 +1,46 @@
 (ns clojure-rest.handler
-      (:import [com.mongodb DB WriteConcern]
-               [org.bson.types ObjectId]
-               [java.util.Date])
-      (:require [compojure.core :refer [defroutes context GET POST]]
-                [ring.util.response :refer [response]]
-                [compojure.handler :as handler]
-                [clj-time.core]
-                [monger.core :as mg]
-                [monger.json]
-                [monger.joda-time]
-                [monger.collection :as mc]
-                [ring.middleware.json :as middleware]
-                [ring.middleware.cors :refer [wrap-cors]]
-                [compojure.route :as route]))
+    (:import [com.mongodb DB WriteConcern]
+             [org.bson.types ObjectId]
+             [java.util.Date])
+    (:require [compojure.core :refer [defroutes context GET POST]]
+              [ring.util.response :refer [response]]
+              [compojure.handler :as handler]
+              [clj-time.core]
+              [monger.core :as mg]
+              [monger.json]
+              [monger.joda-time]
+              [monger.collection :as mc]
+              [ring.middleware.json :as middleware]
+              [ring.middleware.cors :refer [wrap-cors]]
+              [compojure.route :as route]))
 
-    (mg/connect!)
-    (mg/set-db! (mg/get-db "monger-test"))
+  (mg/connect!)
+  (mg/set-db! (mg/get-db "monger-test"))
 
-    (defn get-all-entries []
-      (response {:results (mc/find-maps "entries")}))
 
-    (defn create-new-entry [doc]
-      (let [existing (mc/find-maps "entries" {:title (doc "title")})
-            db_doc (merge {"created_at" (clj-time.core/now)} doc)]
-        (cond
-          (empty? existing) (response (mc/insert-and-return "entries" db_doc))
-          :else {:status 404})))
 
-    (defroutes app-routes
-      (context "/entries" [] (defroutes entries-routes
-        (GET  "/" [] (get-all-entries))
-        (POST "/" {body :body} (create-new-entry body))))
-    ;    (context "/:id" [id] (defroutes document-routes
-    ;      (GET    "/" [] (get-document id))
-    ;      (PUT    "/" {body :body} (update-document id body))
-    ;      (DELETE "/" [] (delete-document id))))))
-      (route/not-found "Not Found"))
+  (defn get-all-entries []
+    (response {:results (mc/find-maps "entries")}))
 
-    (def app
-        (-> (handler/api app-routes)
-            (middleware/wrap-json-body)
-            (wrap-cors :access-control-allow-origin #".*")
-            (middleware/wrap-json-response)))
+  (defn create-new-entry [doc]
+    (let [existing (mc/find-maps "entries" {:title (doc "title")})
+          db_doc (merge {"created_at" (clj-time.core/now)} doc)]
+      (cond
+        (empty? existing) (response (mc/insert-and-return "entries" db_doc))
+        :else {:status 404})))
+
+  (defroutes app-routes
+    (context "/entries" [] (defroutes entries-routes
+      (GET  "/" [] (get-all-entries))
+      (POST "/" {body :body} (create-new-entry body))))
+  ;    (context "/:id" [id] (defroutes document-routes
+  ;      (GET    "/" [] (get-document id))
+  ;      (PUT    "/" {body :body} (update-document id body))
+  ;      (DELETE "/" [] (delete-document id))))))
+    (route/not-found "Not Found"))
+
+  (def app
+      (-> (handler/api app-routes)
+          (middleware/wrap-json-body)
+          (wrap-cors :access-control-allow-origin #".*")
+          (middleware/wrap-json-response)))
