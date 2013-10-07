5a841fe004e69d425bde83508371885069c84e66
Stuart Quin <stuart.quin@gmail.com>
2013-10-07 17:32:26 +0100

    Remove debug

--- api/test/clojure_rest/test/handler.clj
+++ api/test/clojure_rest/test/handler.clj
@@ -5,13 +5,6 @@
         clojure-rest.handler)
   (:require [clojure.test :as test]))
 
-; (deftest test-app
-;   (testing "Entries path"
-;     (binding [retreive-sorted-entries #(str "test")]
-;       (let [response (app (request :get "/entries"))]
-;         (is (= (:body response) "{\"results\":\"test\"}"))
-;         (is (= (:status response) 200))))))
-
 (fact (:status (app (request :get "/entries"))) => 200
       (:body (app (request :get "/entries"))) => "{\"results\":\"test\"}"
   (provided (retreive-sorted-entries) => "test"))
