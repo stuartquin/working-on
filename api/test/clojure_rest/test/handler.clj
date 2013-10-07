f45f6dd0524f8f82d9a1447c43fcaaa577725545
Stuart Quin <stuart.quin@gmail.com>
2013-10-07 17:20:41 +0100

    Test using midje

--- api/test/clojure_rest/test/handler.clj
+++ api/test/clojure_rest/test/handler.clj
@@ -1,12 +1,17 @@
 (ns clojure-rest.test.handler
   (:use clojure.test
-        ring.mock.request  
+        ring.mock.request
+        midje.sweet
         clojure-rest.handler)
   (:require [clojure.test :as test]))
 
-(deftest test-app
-  (testing "Entries path"
-    (binding [retreive-sorted-entries #(str "test")]
-      (let [response (app (request :get "/entries"))]
-        (is (= (:body response) "{\"results\":\"test\"}"))
-        (is (= (:status response) 200))))))
+; (deftest test-app
+;   (testing "Entries path"
+;     (binding [retreive-sorted-entries #(str "test")]
+;       (let [response (app (request :get "/entries"))]
+;         (is (= (:body response) "{\"results\":\"test\"}"))
+;         (is (= (:status response) 200))))))
+
+(fact "Hitting a valid URI should return 200"
+  (let [response (app (request :get "/entries"))]
+    (:status response) => 200))
