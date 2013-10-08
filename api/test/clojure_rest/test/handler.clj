6b9485a692b32ed8537230dfe94fc9cf8782b6f8
Stuart Quin <stuart.quin@gmail.com>
2013-10-08 15:48:47 +0100

    GET and POST tests

--- api/test/clojure_rest/test/handler.clj
+++ api/test/clojure_rest/test/handler.clj
@@ -3,7 +3,8 @@
         ring.mock.request
         midje.sweet
         clojure-rest.handler)
-  (:require [clojure.test :as test]))
+  (:require [clojure.test :as test]
+            [monger.collection :as mc]))
 
 (fact "GET /entries request returns results with midje"
       (prerequisite (connect-db) => true
@@ -11,9 +12,8 @@
       (:body (app (request :get "/entries"))) => "{\"results\":[\"test\"]}"
       (:status (app (request :get "/entries"))) => 200)
         
-
-;(fact "POST /entries saves value"
-;      (:body (app (request :get "/entries"))) => "{\"results\":[\"test\"]}"
-;      (provided
-;        (retreive-sorted-entries) => ["test"] :times 1
-;        (connect-db) => true))
+(fact "POST /entries saves value"
+      (prerequisite (connect-db) => true
+                    (mc/insert-and-return "entries" anything) => "test")
+      (let [req (request :post "/entries" "{\"text\":\"test\"}")]
+        (:body (app (content-type req "application/json"))) => "test"))
