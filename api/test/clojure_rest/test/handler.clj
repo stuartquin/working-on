4bdacbb2e1a875f90bacebcefda7e564b93ce4ba
Stuart Quin <stuart.quin@gmail.com>
2013-10-08 09:13:20 +0100

    Stubs using redefs

--- api/test/clojure_rest/test/handler.clj
+++ api/test/clojure_rest/test/handler.clj
@@ -5,7 +5,7 @@
         clojure-rest.handler)
   (:require [clojure.test :as test]))
 
-(fact (:status (app (request :get "/entries"))) => 200
-      (:body (app (request :get "/entries"))) => "{\"results\":[\"test\"]}"
-  (provided (retreive-sorted-entries) => ["test"] :times 1
-            (connect-db) => [:whatever]))
+(with-redefs [retreive-sorted-entries (fn [] ["test"])
+              connect-db (fn [] true)]
+  (fact (:status (app (request :get "/entries"))) => 200
+        (:body (app (request :get "/entries"))) => "{\"results\":[\"test\"]}"))
