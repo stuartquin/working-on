39733cfad37764f1261a37f68abfbb8fa3587644
Stuart Quin <stuart.quin@gmail.com>
2013-10-08 22:30:12 +0100

    Markdown editor

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -2,6 +2,7 @@
 
 angular.module('WorkingOnApp')
   .controller('MainCtrl', function ($scope, $resource) {
+    $scope.entries = [];
     var setEntriesScope = function(data){
       $scope.entries = data.results;
     };
@@ -9,10 +10,16 @@ angular.module('WorkingOnApp')
     var Entry = $resource("http://local.workingon.com/entries");
     Entry.get({}, setEntriesScope);
 
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/tomorrow");
+    editor.getSession().setMode("ace/mode/markdown");
+    editor.renderer.setShowGutter(false);
+    editor.setFontSize("16px");
+
     $scope.createEntry = function(){
       var entry = new Entry();
-      entry.text = this.newEntry;
+      entry.text = editor.getSession().getValue();
       entry.$save();
-      $scope.entries.unshift({created_at: (new Date()).getTime(), text: this.newEntry});
+      $scope.entries.unshift({created_at: (new Date()).getTime(), text: entry.text});
     };
   });
