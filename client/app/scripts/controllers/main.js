9b983e8ba6365ae8e00a34a84960fb3362bce9f1
Stuart Quin <stuart.quin@gmail.com>
2013-10-13 17:53:23 +0100

    Extract tags and send to server

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -7,6 +7,19 @@ angular.module('WorkingOnApp')
       $scope.entries = data.results;
     };
 
+    var getTagsFromEditor = function(editor){
+      var session = editor.getSession();
+      var tokens = [];
+      for ( var i =0; i < session.getLength(); i++){
+        tokens = tokens.concat(session.getTokens(i));
+      }
+      console.log(tokens);
+      var tokens = tokens.filter(function(token){ 
+        return token.type == "custom.tag";
+      });  
+      return tokens.map(function(token){return token.value.substr(1);});
+    };     
+           
     var Entry = $resource("http://local.workingon.com/entries");
     Entry.get({}, setEntriesScope);
 
@@ -20,7 +33,10 @@ angular.module('WorkingOnApp')
 
     $scope.createEntry = function(){
       var entry = new Entry();
+
       entry.text = editor.getSession().getValue();
+      entry.tags = getTagsFromEditor(editor);
+      console.log(entry);
       entry.$save();
       $scope.entries.unshift({created_at: (new Date()).getTime(), text: entry.text});
     };
